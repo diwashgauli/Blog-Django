06889a3e05b1080271ab8f5c5d19ae19db755426
Diwas Gauli <gaulidiwas@gmail.com>
2025-06-16 16:59:03 +0545

    few changes made in views

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,23 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from blog_app.models import Post
-
+from blog_app.forms import PostForm
 def post_list(request):
-    published_posts = Post.objects.filter(published_at__isnull=False)
-    draft_posts = Post.objects.filter(published_at__isnull=True)
+    published_posts = Post.objects.filter(published_at__isnull=False).order_by('-published_at')
 
-    if published_posts.exists():
-        return render(
+    return render(
             request,
             "post_list.html",
             {"posts": published_posts} 
         )
-    else:
-        return render(
-            request,
-            "drafts.html",
-            {"drafts": draft_posts} 
-        )
-
+   
 
 
 def view_content(request,id):
@@ -29,10 +21,35 @@ def view_content(request,id):
     )
 
 def draft_list(request):
-    draft_posts = Post.objects.filter(published_at__isnull=True)
+    draft_posts = Post.objects.filter(published_at__isnull=True).order_by('-id')
     return render(
         request,
         "drafts.html",
         {"drafts": draft_posts}
+        
     )
+
+
+def post_create(request):
+    if request.method== "GET":
+        form = PostForm()
+        return render(
+            request,
+            "post_create.html",
+            {"form":form}
+                    
+        )
+    else:
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author =request.user #logged in user will be author
+            post.save()
+            return redirect("draft-detail",pk=post.pk)
+        else:
+            return render(
+                request,
+                "post_create.html",
+                {"form":form}
+            )
 
